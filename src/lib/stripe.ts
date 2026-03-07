dec5b28329eb36e9b223dd31148f339591f57667
Pedro <pedronunesphotos@gmail.com>
2026-03-06 23:47:49 -0300

    fix stripe api version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,9 +1,6 @@
-import Stripe from 'stripe';
+import Stripe from 'stripe'
 
-// Singleton para reutilizar a instância do Stripe no servidor
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,
-  
-  {
-  apiVersion: '2026-02-25.clover', // Ajustado conforme a exigência do seu terminal atual
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2023-10-16',
   typescript: true,
-});
+})
