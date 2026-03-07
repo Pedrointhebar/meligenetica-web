a0aa94179ae3340004e8e55659d81e40c5c9a4c8
Pedro <pedronunesphotos@gmail.com>
2026-03-07 00:16:21 -0300

    fix stripe lazy init

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,3 +1,5 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+export function getStripe() {
+  return new Stripe(process.env.STRIPE_SECRET_KEY as string)
+}
